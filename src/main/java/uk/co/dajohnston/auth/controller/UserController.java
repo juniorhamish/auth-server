abc0ce5384e3e01e987350c8aa720df3ecbe817a
David Johnston <davidajohnston2000@gmail.com>
2017-06-07 22:22:51 +0100

    Implement auth server to provide authorization to other apps.

--- src/main/java/uk/co/dajohnston/auth/controller/UserController.java
+++ src/main/java/uk/co/dajohnston/auth/controller/UserController.java
@@ -1,14 +1,18 @@
 package uk.co.dajohnston.auth.controller;
 
 import java.security.Principal;
+import java.util.HashMap;
+import java.util.Map;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserController {
 
-    @RequestMapping("/user")
-    public Principal user(Principal principal) {
-        return principal;
+    @RequestMapping({"/user", "/me"})
+    public Map<String, String> user(Principal principal) {
+        Map<String, String> data = new HashMap<>();
+        data.put("name", principal.getName());
+        return data;
     }
 }
