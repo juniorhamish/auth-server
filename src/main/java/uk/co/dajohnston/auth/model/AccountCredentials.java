a7888f29e6fe0a3a31e15364f14d5654b7631ca1
David Johnston <davidajohnston2000@gmail.com>
2017-06-18 17:32:03 +0100

    Implement authorization with JWT

--- src/main/java/uk/co/dajohnston/auth/model/AccountCredentials.java
+++ src/main/java/uk/co/dajohnston/auth/model/AccountCredentials.java
@@ -2,15 +2,15 @@ package uk.co.dajohnston.auth.model;
 
 public class AccountCredentials {
 
-    private String username;
+    private String emailAddress;
     private String password;
 
-    public String getUsername() {
-        return username;
+    public String getEmailAddress() {
+        return emailAddress;
     }
 
-    public void setUsername(String username) {
-        this.username = username;
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
     }
 
     public String getPassword() {
